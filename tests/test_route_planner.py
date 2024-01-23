776442a75a8199dd3ba8c2d6194947f51bf4b65f
Jacob McIntosh <nacitar.sevaht@gmail.com>
2024-01-23 08:07:47 -0700

    Added test to flag bad damage data

--- tests/test_route_planner.py
+++ tests/test_route_planner.py
@@ -1,5 +1,31 @@
 from route_planner import __version__
+from route_planner.route import HitType
+from route_planner.sl1 import SL1_HIT_LOOKUP
 
 
 def test_version() -> None:
     assert __version__ == "0.1.0"
+
+
+def test_sl1_damage_numbers_look_reasonable() -> None:
+    errors: list[str] = []
+    for weapon, enemy_to_hittype_to_hit in SL1_HIT_LOOKUP.items():
+        for enemy, hittype_to_hit in enemy_to_hittype_to_hit.items():
+            last_damage = 0
+            last_rtsr_damage = 0
+            for hittype in list(HitType):  # enum defined weakest to strongest
+                hit = hittype_to_hit.get(hittype)
+                if hit is None:
+                    continue
+                error_root = f"{weapon} vs {enemy} with {hittype}"
+                if hit.damage < last_damage:
+                    errors.append(f"{error_root} of {hit.damage}")
+                else:
+                    last_damage = hit.damage
+                if hit.with_rtsr < last_rtsr_damage:
+                    errors.append(f"{error_root} of {hit.with_rtsr} with RTSR")
+                else:
+                    last_rtsr_damage = hit.with_rtsr
+    for error in errors:
+        print(f"{error} looks wrong given other provided numbers.")
+    assert len(errors) == 0
